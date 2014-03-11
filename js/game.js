220cd272aaf7add861704c078aabfb0b4da1e7ad
Bob Ippolito <bob@redivi.com>
2014-03-11 13:47:36 -0700

    add an animated player sprite

--- js/game.js
+++ js/game.js
@@ -3,15 +3,17 @@
 
 var game = null;
 var map = null;
-var layers = {};
+var layers = null;
 var gameState = {
     preload: function (game) {
         // Load the area01 map and its tiles
         game.load.tilemap('area01', 'data/map/area01.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('area01_level_tiles', 'data/img/map/area01_level_tiles.png');
+        // Load the main player spritesheet
+        game.load.spritesheet('gripe_run_right', 'data/img/sprite/gripe_run_right.png', 64, 64);
     },
     create: function (game) {
-        game.stage.backgroundColor = '#787878';
+        game.stage.backgroundColor = '#21C3FC';
         
         // Create the map and associate the tiles with it
         map = game.add.tilemap('area01');
@@ -19,11 +21,18 @@ var gameState = {
         
         // Create all of the layers from the tileset,
         // but set visible to false if the name has 'collision' in it.
+        layers = {};
         map.layers.forEach(function (layerData) {
             var layer = map.createLayer(layerData.name);
             layer.visible = !/collision/i.test(layerData.name);
             layers[layerData.name] = layer;
         });
+        
+        var player = game.add.sprite(32*10, 32*4, 'gripe_run_right');
+        // Use all of the frames for the 'walk' animation
+        player.animations.add('walk');
+        // Play the walk animation at 20fps and loop
+        player.animations.play('walk', 15, true);
     }
 };
 
